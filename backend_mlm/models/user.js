b4523d08efd5f50235775bd0445a24a0bea09fcb
Rejith Reghunathan <rejithreghunathan6378@gmail.com>
2021-02-18 11:38:10 +0530

    User Model completed

--- backend_mlm/models/user.js
+++ backend_mlm/models/user.js
@@ -1,22 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        maxlength:32,
-        trim:true
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
     },
-    lastname:{
-        type:String,
-        maxlength:32,
-        trim:true,
-        required:true
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+      required: true,
     },
-    email:{
-        type:String,
-        trim:true,
-        required:true,
-        unique:true
-    }
-})
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    secured_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {timestamps:true}
+);
+
+
+module.exports = mongoose.model('User', userSchema)
